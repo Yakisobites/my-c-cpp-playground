9f9502a85feecfc53fdbed4bc47fd377c3174246
Naoya <naoyakp10@outlook.jp>
2026-08-23 00:40:49 +0900

    feat: create add.c, add.h, main.c, and add_test.cpp

---
+++ tests/add_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+extern "C"
+{
+#include "add.h"
+}
+
+TEST(CoreAddTest, AddsPositiveNumbers)
+{
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(CoreAddTest, AddsNegativeNumbers)
+{
+    EXPECT_EQ(add(-2, -3), -5);
+}
