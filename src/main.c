e932a06dc8e5cfb0b79922e6af50e918e3de1268
Naoya <naoyakp10@outlook.jp>
2026-08-23 01:08:09 +0900

    edit settings.json to add includePath for add.h and add.c

--- src/main.c
+++ src/main.c
@@ -2,9 +2,8 @@
 
 #include "add.h"
 
-int main(void)
-{
-    int result = add(2, 3);
-    printf("Hello from CMake C project. 2 + 3 = %d\n", result);
-    return 0;
+int main(void) {
+  int result = add(2, 3);
+  printf("Hello from CMake C project. 2 + 3 = %d\n", result);
+  return 0;
 }
--- tests/add_test.cpp
+++ tests/add_test.cpp
@@ -1,16 +1,9 @@
 #include <gtest/gtest.h>
 
-extern "C"
-{
+extern "C" {
 #include "add.h"
 }
 
-TEST(CoreAddTest, AddsPositiveNumbers)
-{
-    EXPECT_EQ(add(2, 3), 5);
-}
+TEST(CoreAddTest, AddsPositiveNumbers) { EXPECT_EQ(add(2, 3), 5); }
 
-TEST(CoreAddTest, AddsNegativeNumbers)
-{
-    EXPECT_EQ(add(-2, -3), -5);
-}
+TEST(CoreAddTest, AddsNegativeNumbers) { EXPECT_EQ(add(-2, -3), -5); }
